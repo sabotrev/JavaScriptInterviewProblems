c3f4c53daa37e51434205d70c70e24fa67943a33
Trevor Sabo <sabotrev@gmail.com>
2022-03-05 19:08:16 -0800

    Added Verify Alien Dictionary

--- ArraysAndStrings/MonotoneIncreasing.js
+++ ArraysAndStrings/MonotoneIncreasing.js
@@ -6,21 +6,24 @@ const minFlipsMonoIncr = (s) => {
         return 0;
     }
 
-    let N = s.length;
-    let prefixSum = new Array(N + 1).fill(0);
-    for (let i = 0; i < N; i++) {
-        prefixSum[i + 1] = prefixSum[i] + (s[i] === '1' ? 1 : 0);
+    let prefixOnes = new Array(s.length + 1).fill(0);
+    let postfixZeroes = new Array(s.length + 1).fill(0);
+    for (let i = 0; i < s.length; i++) {
+        prefixOnes[i + 1] = prefixOnes[i] + (s[i] === '1' ? 1 : 0);
     }
-    console.log(prefixSum);
+    for (let i = s.length - 1; i >= 0; i--) {
+        postfixZeroes[i] = postfixZeroes[i + 1] + (s[i] === '0' ? 1 : 0);
+    }
 
-    let ans = Infinity;
-    for (let j = 0; j <= N; j++) {
-        let ones = prefixSum[j];
-        let zeroes = N - j - (prefixSum[N] - prefixSum[j]);
-        console.log(`ones count: ${ones}, zeroes Count: ${zeroes}`);
-        ans = Math.min(ones + zeroes, ans);
+    console.log(prefixOnes);
+    console.log(postfixZeroes);
+    let result = Infinity;
+    for (let i = 0; i <= s.length; i++) {
+        result = Math.min(result, prefixOnes[i] + postfixZeroes[i]);
     }
-    return ans;
+
+    return result;
 };
 
-console.log(minFlipsMonoIncr('010110'));
+// console.log(minFlipsMonoIncr('010110')); // 2
+console.log(minFlipsMonoIncr('00011000')); // 2
